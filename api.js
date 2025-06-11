cbdc561d61a35f2dc8b89ca00cd9a6169a267397
Ivan Silva Alves <139565955+Ivan-ssa@users.noreply.github.com>
2025-06-11 13:01:24 -0300

    Update api.js

--- api.js
+++ api.js
@@ -17,4 +17,5 @@ app.get("/contador", (req, res) => {
   res.json({ contador });
 });
 
-app.listen(3000, () => console.log("API rodando na porta 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
